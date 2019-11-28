1d53ae8c8e2c29d2881d30849d3af3e5c82d55cb
thanhoangz <xuanhoang.ks6@gmail.com>
2019-11-28 08:33:29 +0700

    fix dần di

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'trangtiep',
+    component: LoginComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
